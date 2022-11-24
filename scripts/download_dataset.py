3cf52e0174099fb5a85bf360b60e2deec9cd69da
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-11-24 20:41:58 +0600

    Copy dataset downloading template from old project

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -1,13 +1,9 @@
 from pathlib import Path
+from zipfile import ZipFile
 
 import gdown
 
-from const import DATA_DIR
-
-try:
-    from zipfile import ZipFile
-except ImportError:
-    from zipfile36 import ZipFile
+from const import INPUT_DIR
 
 
 def unzip(archives_dir: Path) -> None:
@@ -19,16 +15,16 @@ def unzip(archives_dir: Path) -> None:
 
 def load_data() -> None:
     gdown.cached_download(
-        id='1tYTSAYKMF_gHV542tSwKighLIoNImE1g',
-        path=str(DATA_DIR / 'subset_masks.zip'),
+        url='1tYTSAYKMF_gHV542tSwKighLIoNImE1g',
+        path=str(INPUT_DIR / 'subset_masks.zip'),
         md5='140515cb3f4942e3baf77645115e88b8'
     )
     gdown.cached_download(
-        id='1tYTSAYKMF_gHV542tSwKighLIoNImE1g',
-        path=str(DATA_DIR / 'subset_img.zip'),
+        url='1tYTSAYKMF_gHV542tSwKighLIoNImE1g',
+        path=str(INPUT_DIR / 'subset_img.zip'),
         md5='cd247ca63549f7b281fb01a718f89fc5',
     )
-    unzip(archives_dir=DATA_DIR)
+    unzip(archives_dir=INPUT_DIR)
 
 
 if __name__ == '__main__':
