98935d8bfe146bc025ebbb773939255205286de0
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-11-26 12:11:42 +0600

    Add smp.Unet model

--- src/model.py
+++ src/model.py
@@ -1,3 +1,26 @@
 """
-UNet3D
+Models for segmentation
 """
+import torch
+import segmentation_models_pytorch as smp
+
+class UnetSMPWrapper(torch.nn.Module):
+    """ Wrapper for smp.Unet """
+
+    def __init__(self, in_channels: int, encoder_name: str = 'resnet34') -> None:
+        """ Create model from segmentation_models_pytorch """
+        super().__init__()
+        self.model = smp.Unet(
+            encoder_name=encoder_name,
+            encoder_weights="imagenet",
+            decoder_channels=[256, 128, 64, 32, 64],
+            in_channels=in_channels,
+            classes=in_channels,
+            aux_params=None,
+            activation=None
+        )
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        """ Run model on image """
+        result = self.model(image)
+        return torch.sigmoid(result)
