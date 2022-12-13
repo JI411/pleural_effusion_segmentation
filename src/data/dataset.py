9a685a730e4d8079c33fb62122929cb405c4ce6c
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-12-13 11:18:34 +0600

    Add rotation

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -11,6 +11,7 @@ class PleuralEffusionDataset3D(PleuralEffusionDataset):
     def __getitem__(self, idx: int) -> Batch:
         """ Get lung image and mask for it """
         image = list(read_data.load_dicom_recursive(self.image_dir_paths[idx]))[0]
+        image = preprocessing.rotate_array(image)
         mask = read_data.load_mask_from_dir(self.masks_dir_paths[idx])
         image = preprocessing.normalize(image)
         return Batch(image=image.astype('float32'), mask=mask.astype(int))
