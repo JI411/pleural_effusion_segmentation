45670e093b3bf24511b3769c2b3d7ddafe202b7b
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-12-13 18:53:24 +0600

    Add inference for smp unet

--- run.py
+++ run.py
@@ -23,7 +23,7 @@ def main(args):
         trainer = Trainer(auto_scale_batch_size='power')
         batch_size = trainer.tune(model)['scale_batch_size']
 
-    model = PleuralSegmentationModule(model=wrappers.Unet3DWrapper(), batch_size=batch_size)
+    model = PleuralSegmentationModule(model=wrappers.UnetSMPWrapper(), batch_size=batch_size)
     wandb_logger = WandbLogger(project="pleural_effusion_segmentation", save_dir=const.LOG_DIR)
     wandb_logger.watch(model)
     trainer = Trainer.from_argparse_args(args, logger=wandb_logger, profiler="simple", callbacks=[DeviceStatsMonitor()])  # todo
