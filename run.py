ce31e35c8e6a72f6ac4b8899c0cb2eaed916b29a
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-12-13 17:38:38 +0600

    Add inference for smp unet

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ from src.train import PleuralSegmentationModule
 
 
 def main(args):
+    from pytorch_lightning.callbacks import DeviceStatsMonitor
     """Run training pipline."""
     seed_everything(const.SEED, workers=True)
 
@@ -25,7 +26,7 @@ def main(args):
     model = PleuralSegmentationModule(model=wrappers.Unet3DWrapper(), batch_size=batch_size)
     wandb_logger = WandbLogger(project="pleural_effusion_segmentation", save_dir=const.LOG_DIR)
     wandb_logger.watch(model)
-    trainer = Trainer.from_argparse_args(args, logger=wandb_logger)
+    trainer = Trainer.from_argparse_args(args, logger=wandb_logger, profiler="simple", callbacks=[DeviceStatsMonitor()])
     trainer.fit(model)
 
 
