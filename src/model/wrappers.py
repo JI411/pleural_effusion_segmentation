76c8bcdf199520212f829e28a0c804cfcb7a4011
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-12-13 16:56:02 +0600

    Add inference for smp unet

--- src/model/wrappers.py
+++ src/model/wrappers.py
@@ -3,6 +3,7 @@ Models for segmentation
 """
 
 import segmentation_models_pytorch as smp
+import torch
 from pytorch3dunet.unet3d.model import UNet2D, UNet3D, ResidualUNet3D
 
 from src.model.base import BaseModel
@@ -37,15 +38,28 @@ class ResidualUNet3DWrapper(BaseModel):
 
 class UnetSMPWrapper(BaseModel):
     """ Wrapper for smp.Unet model """
-    def __init__(self, in_channels: int, encoder_name: str = 'resnet34') -> None:
+
+    in_channels: int = 1
+
+    def __init__(self, encoder_name: str = 'resnet34') -> None:
         """ Create segmentation_models_pytorch.Unet model """
-        super().__init__(in_channels=in_channels)
+        super().__init__(in_channels=self.in_channels)
         self.model = smp.Unet(
             encoder_name=encoder_name,
             encoder_weights="imagenet",
             decoder_channels=[256, 128, 64, 32, 64],
-            in_channels=in_channels,
-            classes=in_channels,
+            in_channels=self.in_channels,
+            classes=self.in_channels,
             aux_params=None,
             activation=None
         )
+
+    def forward(self, image: torch.Tensor) -> torch.Tensor:
+        """ Run model on image/batch """
+        result = []
+        for tensor_slice in torch.moveaxis(image, 2, 0):
+            pred = self.model(tensor_slice)
+            result.append(pred)
+
+        result = torch.cat(result, dim=1)
+        return result.unsqueeze(1)
