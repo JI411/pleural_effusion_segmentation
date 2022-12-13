549288eb52e0d0379e0b666534b08da52aad70f5
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-12-13 18:50:34 +0600

    Add inference for smp unet

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -14,4 +14,4 @@ class PleuralEffusionDataset3D(PleuralEffusionDataset):
         image = preprocessing.rotate_array(image)
         mask = read_data.load_mask_from_dir(self.masks_dir_paths[idx])
         image = preprocessing.normalize(image)
-        return Batch(image=image.astype('float32')[:3, ...], mask=mask.astype(int)[:1, ...])  # todo remove
+        return Batch(image=image.astype('float32')[:10, ...], mask=mask.astype(int)[:1, ...])  # todo remove
