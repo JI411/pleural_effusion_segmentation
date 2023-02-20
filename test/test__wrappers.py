8180ae5a555df08ea30e5187edc18b93cb1e21bc
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-02-21 02:19:43 +0600

    Remove old components

--- test/test__wrappers.py
+++ test/test__wrappers.py
@@ -54,7 +54,6 @@ def _random_sphere_dataloader(shape: tp.Sequence[int]) -> DataLoader:
 
 @pytest.mark.parametrize(
     "input_shape,num_epoch,net,max_allowed_loss", [
-        ((4, 1, 16, 32, 32), 20, wrappers.Unet3DWrapper(), 0.025),
         ((4, 1, 128, 128), 20, wrappers.UnetSMP2DWrapper(), 0.025),
     ]
 )
