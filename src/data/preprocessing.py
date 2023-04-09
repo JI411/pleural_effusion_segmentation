c4be66bf3aedb179ec90c477aec6183fd233027d
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-04-09 19:57:26 +0600

    Update augmentations

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -11,7 +11,7 @@ def train_augmentation() -> vol.Compose:
 
     return vol.Compose([
         # vol.RandomCropFromBorders(crop_value=0.1, p=0.1),
-        vol.ElasticTransform((0, 0.2), interpolation=2, p=0.1),
+        # vol.ElasticTransform((0, 0.2), interpolation=2, p=0.1),
         # vol.Rotate((-10, 10), (-5, 5), (-5, 5), p=0.1),
         vol.Resize(
             PATCH_SIZE,
@@ -21,7 +21,7 @@ def train_augmentation() -> vol.Compose:
             p=1.
         ),
         vol.GaussianNoise(var_limit=(0, 10), p=0.3),
-        # vol.Flip(1, p=0.2),
+        vol.Flip(0, p=0.2),
         vol.Normalize(range_norm=False, p=1.),
     ], p=1.)
 
