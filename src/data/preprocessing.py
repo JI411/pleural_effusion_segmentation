18329bedee4eb76520dd16169fa30a3be19e7155
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2023-04-10 04:44:54 +0600

    Update augmentations

--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -9,23 +9,26 @@ from const import PATCH_SIZE
 def train_augmentation() -> vol.Compose:
     """Train augmentations."""
     return vol.Compose([
-        vol.Rotate((-5, 5), (-2, 2), (-2, 2), p=0.3),
+        vol.Normalize(range_norm=False, p=1.),
+        vol.Rotate((-5, 5), (-2, 2), (-2, 2), border_mode='nearest', p=0.05),
         vol.RandomCropFromBorders(crop_value=0.05, p=0.05),
         vol.Resize(
             PATCH_SIZE,
             interpolation=1,
             resize_type=0,
             always_apply=True,
-            p=1.
+            p=1.,
         ),
         vol.Flip(0, p=0.01),
-        vol.GaussianNoise(var_limit=(10, 40), p=0.4),
+        vol.RandomScale(scale_limit=[0.999, 1.001], p=0.05),
+        vol.GaussianNoise(var_limit=(0., 0.1), p=0.2),
         vol.Normalize(range_norm=False, p=1.),
     ], p=1.)
 
 def valid_augmentation() -> vol.Compose:
     """Validation augmentations."""
     return vol.Compose([
+        vol.Normalize(range_norm=False, p=1.),
         vol.Resize(
             PATCH_SIZE,
             interpolation=1,
